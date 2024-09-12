ea17e56b23ed7f2d205b6291a063e1219fd2ad5c
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-09-12 10:15:25 -0700

    cwercwerwecrwe

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,21 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+double numOne = 19.9234;
+double numTwo = 20.2912;
+
+int roundedOne = (int) (numOne + 0.5);
+int roundedTwo = (int) (numTwo + 0.5);
+
+System.out.print(roundedOne + roundedTwo);
+
+
+
+
+
+
+
+  }
+}
 
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,12 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+  double num = 69.3424
+  int numTwo = (int) (num * 1000);
+
+  
+  }
+}
+
 
